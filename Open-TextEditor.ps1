13fbdbbf7a0d1f0b82d2c206e1166e0cb6cfe4a6
Rafael Goodman <rafael@getthefrog.com>
2014-05-07 14:49:29 -0400

    Make Open-TextEditor ISE-aware

--- Open-TextEditor.ps1
+++ Open-TextEditor.ps1
@@ -1,4 +1,27 @@
 param([string]$file="")
 
+$psExtensions = @(
+    '.ps1'
+    '.psm1'
+    '.psd1'
+)
+
+if($file -and -not (Test-Path $file))
+{
+    '' | Set-Content -Path $file -Encoding Ascii
+}
+
+if(Test-Path variable:\psISE)
+{
+    psEdit $file
+    return
+}
+
+if($psExtensions -contains [System.IO.Path]::GetExtension($file))
+{
+    & PowerShell_ISE.exe $file
+    return
+}
+
 $texteditor = Get-TextEditor;
 Invoke-Expression "&`"$texteditor`" `"$file`""
