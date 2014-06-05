bd02c511bb58150ce347511131d57e81a83afb62
Rafael Goodman <rafael@getthefrog.com>
2014-06-05 10:25:54 -0400

    Restored correct version of Sot-PictureIntoDirectory

--- Sort-PictureIntoDirectory.ps1
+++ Sort-PictureIntoDirectory.ps1
@@ -15,32 +15,46 @@ function addDateTakenMember
 		$dateTaken = $null   
 		$fileStream = $null
 		$image = $null
-		try
-		{
-			$fileStream = new-object System.IO.FileStream $FileInfo.FullName, "Open", "Read"
-			$image = [System.Drawing.Image]::FromStream($fileStream, $false, $false)
-			$dateTakenString = [System.Text.Encoding]::UTF8.GetString($image.GetPropertyItem(36867).Value)
-			$dateTakenString = ($dateTakenString -split " ")[0]
-			$dateTakenString = $dateTakenString -replace ":","-"
-			$dateTaken = [datetime]$dateTakenString    
-		}
-		catch
-		{
-		}
-		finally
-		{
-			if($null -ne $image)
-			{
-				$image.Dispose()
-				$image = $null        
-			}
 
-			if($null -ne $fileStream)
-			{
-				$fileStream.Dispose()
-				$fileStream = $null
-			}
-		}
+        if($FileInfo.Name -match '^wp_(?<timestamp>\d\d\d\d\d\d\d\d_\d\d\d\d\d\dZ)\.mp4$')
+        {
+            $dateTakenString = $Matches['timestamp']
+            $dateTaken = [datetime]::ParseExact($dateTakenString, 'yyyyMMdd_HHmmssZ', [System.Globalization.CultureInfo]::CurrentCulture)
+        }
+        elseif($FileInfo.Name -match '^wp_(?<timestamp>\d\d\d\d\d\d\d\d_\d\d_\d\d_\d\d).*?\.mp4$')
+        {
+            $dateTakenString = $Matches['timestamp']
+            $dateTaken = [datetime]::ParseExact($dateTakenString, 'yyyyMMdd_HH_mm_ss', [System.Globalization.CultureInfo]::CurrentCulture)
+        }
+        else
+        {
+		    try
+		    {
+			    $fileStream = new-object System.IO.FileStream $FileInfo.FullName, "Open", "Read"
+			    $image = [System.Drawing.Image]::FromStream($fileStream, $false, $false)
+			    $dateTakenString = [System.Text.Encoding]::UTF8.GetString($image.GetPropertyItem(36867).Value)
+			    $dateTakenString = ($dateTakenString -split " ")[0]
+			    $dateTakenString = $dateTakenString -replace ":","-"
+			    $dateTaken = [datetime]$dateTakenString    
+		    }
+		    catch
+		    {
+		    }
+		    finally
+		    {
+			    if($null -ne $image)
+			    {
+				    $image.Dispose()
+				    $image = $null        
+			    }
+
+			    if($null -ne $fileStream)
+			    {
+				    $fileStream.Dispose()
+				    $fileStream = $null
+			    }
+		    }
+        }
 		
 		Add-Member -InputObject $FileInfo -MemberType NoteProperty -Name DateTaken -Value $dateTaken -PassThru
 	}
@@ -50,7 +64,13 @@ dir |
 	where { -not $_.PSIsContainer } |
 	addDateTakenMember |
 	foreach {
-		$destDir = "$($_.DateTaken.Year)-$($_.DateTaken.Month.ToString('D2'))-$($_.DateTaken.Day.ToString('D2'))"
+		$date = $_.CreationTime
+		if($_.DateTaken)
+		{
+			$date = $_.DateTaken
+		}
+
+		$destDir = "$($date.Year)-$($date.Month.ToString('D2'))-$($date.Day.ToString('D2'))"
 		if(-not (Test-Path $destDir -PathType "Container"))
 		{
 			Write-Host "Creating directory: $destDir"
