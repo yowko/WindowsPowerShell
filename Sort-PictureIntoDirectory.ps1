6d8258af826bd1ba6255af1357b202a8b5e80a65
Rafael Goodman <rafael@getthefrog.com>
2014-08-04 10:11:10 -0400

    Use UTC for Sort-PictureIntoDirectory; use EXIF data before file name parsing

--- Sort-PictureIntoDirectory.ps1
+++ Sort-PictureIntoDirectory.ps1
@@ -16,44 +16,50 @@ function addDateTakenMember
 		$fileStream = $null
 		$image = $null
 
-        if($FileInfo.Name -match '^wp_(?<timestamp>\d\d\d\d\d\d\d\d_\d\d\d\d\d\dZ)\.mp4$')
+		try
+		{
+			$fileStream = new-object System.IO.FileStream $FileInfo.FullName, "Open", "Read"
+			$image = [System.Drawing.Image]::FromStream($fileStream, $false, $false)
+			$dateTakenString = [System.Text.Encoding]::UTF8.GetString($image.GetPropertyItem(36867).Value)
+			$dateTakenString = ($dateTakenString -split " ")[0]
+			$dateTakenString = $dateTakenString -replace ":","-"
+			$dateTaken = [datetime]$dateTakenString    
+		}
+        catch
         {
-            $dateTakenString = $Matches['timestamp']
-            $dateTaken = [datetime]::ParseExact($dateTakenString, 'yyyyMMdd_HHmmssZ', [System.Globalization.CultureInfo]::CurrentCulture)
         }
-        elseif($FileInfo.Name -match '^wp_(?<timestamp>\d\d\d\d\d\d\d\d_\d\d_\d\d_\d\d).*?\.mp4$')
-        {
-            $dateTakenString = $Matches['timestamp']
-            $dateTaken = [datetime]::ParseExact($dateTakenString, 'yyyyMMdd_HH_mm_ss', [System.Globalization.CultureInfo]::CurrentCulture)
-        }
-        else
-        {
-		    try
-		    {
-			    $fileStream = new-object System.IO.FileStream $FileInfo.FullName, "Open", "Read"
-			    $image = [System.Drawing.Image]::FromStream($fileStream, $false, $false)
-			    $dateTakenString = [System.Text.Encoding]::UTF8.GetString($image.GetPropertyItem(36867).Value)
-			    $dateTakenString = ($dateTakenString -split " ")[0]
-			    $dateTakenString = $dateTakenString -replace ":","-"
-			    $dateTaken = [datetime]$dateTakenString    
-		    }
-		    catch
-		    {
-		    }
-		    finally
-		    {
-			    if($null -ne $image)
-			    {
-				    $image.Dispose()
-				    $image = $null        
-			    }
+		finally
+		{
+			if($null -ne $image)
+			{
+				$image.Dispose()
+				$image = $null        
+			}
 
-			    if($null -ne $fileStream)
-			    {
-				    $fileStream.Dispose()
-				    $fileStream = $null
-			    }
-		    }
+			if($null -ne $fileStream)
+			{
+				$fileStream.Dispose()
+				$fileStream = $null
+			}
+		}
+
+        if(-not $dateTaken)
+        {
+            if($FileInfo.Name -match '^wp_(?<timestamp>\d\d\d\d\d\d\d\d_\d\d\d\d\d\dZ)\.mp4$')
+            {
+                $dateTakenString = $Matches['timestamp']
+                $dateTaken = [datetime]::ParseExact($dateTakenString, 'yyyyMMdd_HHmmssZ', [System.Globalization.CultureInfo]::InvariantCulture, 'AssumeUniversal')
+            }
+            elseif($FileInfo.Name -match '^wp_(?<timestamp>\d\d\d\d\d\d\d\d_\d\d_\d\d_\d\d).*?\.mp4$')
+            {
+                $dateTakenString = $Matches['timestamp']
+                $dateTaken = [datetime]::ParseExact($dateTakenString, 'yyyyMMdd_HH_mm_ss', [System.Globalization.CultureInfo]::InvariantCulture, 'AssumeUniversal')
+            }
+            elseif($FileInfo.Name -match '^(?<timestamp>\d\d\d\d\d\d\d\d_\d\d\d\d\d\d)\d\d\d_iOS\..*?$')
+            {
+                $dateTakenString = $Matches['timestamp']
+                $dateTaken = [datetime]::ParseExact($dateTakenString, 'yyyyMMdd_HHmmss', [System.Globalization.CultureInfo]::InvariantCulture, 'AssumeUniversal')
+            }
         }
 		
 		Add-Member -InputObject $FileInfo -MemberType NoteProperty -Name DateTaken -Value $dateTaken -PassThru
@@ -64,11 +70,13 @@ dir |
 	where { -not $_.PSIsContainer } |
 	addDateTakenMember |
 	foreach {
-		$date = $_.CreationTime
-		if($_.DateTaken)
+		if(-not ($_.DateTaken))
 		{
-			$date = $_.DateTaken
+            Write-Warning "DateTaken was not found for $($_.Name)"
+			return
 		}
+
+        $date = $_.DateTaken
 
 		$destDir = "$($date.Year)-$($date.Month.ToString('D2'))-$($date.Day.ToString('D2'))"
 		if(-not (Test-Path $destDir -PathType "Container"))
