12dbbc715b505974f0b10c3e417ced48c4f30e5d
Rafael Goodman <rafael@getthefrog.com>
2015-02-24 19:10:56 -0500

    Remove use of ISE for Open-TextEditor

--- Open-TextEditor.ps1
+++ Open-TextEditor.ps1
@@ -1,11 +1,5 @@
 param([string]$file="")
 
-$psExtensions = @(
-    '.ps1'
-    '.psm1'
-    '.psd1'
-)
-
 if($file -and -not (Test-Path $file))
 {
     '' | Set-Content -Path $file -Encoding Ascii
@@ -23,13 +17,6 @@ if(Test-Path variable:\PGSE)
 {
     $PGSE.DocumentWindows.Add($file)
     return 
-}
-
-if($psExtensions -contains [System.IO.Path]::GetExtension($file))
-{
-    $powershellEditor = Get-PowerShellEditor.ps1
-    & $powershellEditor $file
-    return
 }
 
 $texteditor = Get-TextEditor;
